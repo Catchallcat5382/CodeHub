9305665f81ceede640f738f69edb90a42abfba7e
Ilan Goldstein <ilansheagoldstein@gmail.com>
2026-07-01 21:52:38 -0400

    CodeHub update

--- code_hub.py
+++ code_hub.py
@@ -68,7 +68,7 @@ GITHUB_API_MAIN_SOURCE = f"https://api.github.com/repos/{GITHUB_REPO}/contents/c
 GITHUB_EXE_URL = f"https://github.com/{GITHUB_REPO}/releases/latest/download/CodeHub.exe"
 GITHUB_MAIN_EXE_URL = f"https://github.com/{GITHUB_REPO}/raw/main/CodeHub.exe"
 BUILD_COMMIT = "local-build"
-BUILD_NUMBER = 55
+BUILD_NUMBER = 57
 MAX_REPLAY_FPS = 240
 REPLAY_FPS_CHOICES = ["15", "20", "24", "30", "60", "120", "144", "240"]
 
@@ -6958,6 +6958,7 @@ CodeHub • Built by Catchallcat5382
 if __name__ == "__main__":
     startup_console()
     CodeHubApp().run()
+
 
 
 
